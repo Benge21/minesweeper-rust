dc738a6fe5de4478794476d121d6571f3dbdc461
wachbe <benjamin.wachter@wallawalla.edu>
2026-09-02 06:46:39 -0700

    populate neighbor numbers

--- game_board.rs
+++ game_board.rs
@@ -16,7 +16,7 @@ pub mod game_board {
                 _ => (9, 9, 8),
             };
 
-            let mut board = vec![vec![0; w as usize]; h as usize];
+            let board      = vec![vec![0; w as usize]; h as usize];
             let mut master = vec![vec![0; w as usize]; h as usize];
 
             let mut rng = rand::rng();
@@ -29,20 +29,38 @@ pub mod game_board {
                 if master[row][col] == 0 {
                     master[row][col] = 9;
                     placed += 1;
-
+                    unsafe {
+                        for i in row-1..row+1 { // row
+                            for j in col-1..col+1 { // col
+                                master[i][j] += 1;
+                            };
+                        };
+                    }
                 }
             }
 
             Self { option, board, master }
         }
-        fn populate_neighbors(row: usize, col: usize, board: Vec<_>) {
-            let range = match (row, col) {
-                (1, 1) => [[row,row+1],[col,col+1]],
-                (1, 1) => {
-                    [[row,row+1],[col-1,col+1]]
-                },
-            };
-        }
+        // fn populate_neighbors(row: usize, col: usize, master: Vec<Vec<i8>>) {
+        //     let row_max = self.master.len() - 1;
+        //     let col_max = self.master[0].len() - 1;
+        //     let range = match (row, col) {
+        //         (0, 0)                           => [[row,row+1],[col,col+1]],
+        //         (0, col_max)              => [[row,row+1],[col-1,col]],
+        //         (row_max, 0)              => [[row-1,row],[col,col+1]],
+        //         (row_max, col_max) => [[row-1,row],[col-1,col]],
+        //         (row_max, _)              => [[row-1,row],[col-1,col+1]],
+        //         (0, _)                           => [[row,row+1],[col-1,col+1]],
+        //         (_, 0)                           => [[row-1,row+1],[col,col+1]],
+        //         (_, col_max)              => [[row-1,row+1],[col-1,col]],
+        //         _ => [[row-1,row+1],[col-1,col+1]],
+        //     };
+        //     for i in range[0][0]..range[0][1] { // row
+        //         for j in range[1][0]..range[1][1] { // col
+        //             self.master[i][j] += 1;
+        //         };
+        //     };
+        // }
 
         pub fn get_board(&self) -> &Vec<Vec<i8>> {
             &self.board
---
+++ readme.md
@@ -0,0 +1,4 @@
+
+# Compile
+
+` cargo build `
