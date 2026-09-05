92bcec2bdf0333a4062333d739d3a4705feca747
benge21 <benneybo2@gmail.com>
2026-09-05 09:24:07 -0700

    adding user input

--- main.rs
+++ main.rs
@@ -1,10 +1,15 @@
 mod game_board;
 
+
 fn main() {
 
     let gb = game_board::game_board::GameBoard::new(1);
 
-    println!("{:?}", gb.get_board());
-    println!("{:?}", gb.get_master()); 
-
+    loop {
+        let input =std::io::stdin().read_line(input);
+        match input {
+            "q" => std::process::exit(1),
+            _ => println!("Enter a valid input.")
+        };
+    }
 }
